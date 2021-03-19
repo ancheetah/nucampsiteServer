ffff13ffa941030ce10479e6b02516c215ff5837
AJ <ancheetah@users.noreply.github.com>
2021-03-18 21:56:34 -0400

    cookies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');  
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -35,30 +35,42 @@ app.set('view engine', 'jade');
 app.use(logger('dev')); // Morgan middleware looks at request header and logs it to console
 app.use(express.json());  // Then it passes req and res objs to this middleware which parses the req body
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321')); // can be any string. this key will be used by cookieParser to encrypt info and sign the cookie
 
 // Authenticate users before they can access resources on server
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if(!authHeader) { // user has not input any credentials
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic'); // Tells client server is requesting basic authorization
-    err.status = 401;
-    return next(err);
-  }
+  if (!req.signedCookies.user) {
 
-  // 'Basic' in header contains username and password in a 64-based encoded string. Let's decode it.
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
-    return next(); // authorized. move on to next middleware func
+    const authHeader = req.headers.authorization;
+    if(!authHeader) { // user has not input any credentials
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic'); // Chalenge the client for credentials. Tells client server is requesting basic authorization
+      err.status = 401;
+      return next(err);
+    }
+
+    // 'Basic' in header contains username and password in a 64-based encoded string. Let's decode it.
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', {signed: true});  // creates a new cookie with a name of 'user', a value of 'admin' 
+                                                    // to store in the name property, and an optional config property with tells Express to use the secret key from cookieParser to create a signed cookie
+      return next(); // authorized. move on to next middleware func
+    } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if (req.signedCookies.user === 'admin') {
+        return next();
+    } else {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } 
   }
 }
 
