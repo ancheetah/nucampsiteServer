059a0f50b5c5a9ff854ae654420e760d5b00aa22
AJ <ancheetah@users.noreply.github.com>
2021-03-28 14:37:06 -0400

    delete favorites

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -36,7 +36,6 @@ favoriteRouter.route('/')
                 user: req.user.id,
                 campsites: req.body
             })
-            favorite.save()
             .then(favorite => {
                 console.log('Favorite Created ', favorite);
                 res.json(favorite);
@@ -46,38 +45,32 @@ favoriteRouter.route('/')
     })
 })
 .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
-    // res.status(403).end('PUT operation not supported on /partners');
+    res.status(403).end('PUT operation not supported on /favorites');
 })
 .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    // Partner.deleteMany()
-    // .then(response => {
-    //     res.json(response);
-    // })
-    // .catch(err => next(err));
+    Favorite.findOneAndDelete({ user: req.user._id })
+    .then( favorite => {
+        if (favorite) {
+            console.log('Deleted favorite', favorite)
+            res.json(favorite);
+        } else {
+            res.end('You do not have any favorites to delete.');
+        }
+    })
+    .catch(err => next(err));
 });
 
 //===
 favoriteRouter.route('/:campsiteId')
 .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
 .get((req, res, next) => {
-    res.status(200);
-    // Favorite.findById(req.params.campsiteId)
-    // .then(favorite => {
-    //     res.json(favorite);
-    // })
-    // .catch(err => next(err));
+    res.status(403).end('GET operation not supported on /favorites');
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
     // res.status(403).end(`POST operation not supported on /partners/${req.params.partnerId}`);
 })
 .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    // Partner.findByIdAndUpdate(req.params.partnerId, {
-    //     $set: req.body
-    // }, { new: true })
-    // .then(partner => {
-    //     res.json(partner);
-    // })
-    // .catch(err => next(err));
+    res.status(403).end(`PUT operation not supported on /favorites/${req.params.campsiteId}`);
 })
 .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     // Partner.findByIdAndDelete(req.params.partnerId)
